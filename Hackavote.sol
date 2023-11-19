48d33601d6a6305c572d59301298c1f81d6028af
Ali Mahdiyar <alimahdiyar77@gmail.com>
2023-11-19 07:51:47 +0300

    feat: handle start and end time of the voting

--- Hackavote.sol
+++ Hackavote.sol
@@ -4,8 +4,21 @@ pragma solidity ^0.8.4;
 contract Hackavote {
     mapping(address => bytes[]) private opinions;
     address[] public hackers;
+    uint256 public startTime;
+    uint256 public endTime;
+
+    constructor(uint256 _startTime, uint256 _endTime) {
+        // Ensure start time is in the future and end time is after start time.
+        require(_endTime > _startTime, "Invalid time range.");
+        startTime = _startTime;
+        endTime = _endTime;
+    }
 
     function submitOpinion(bytes memory opinion) public {
+        require(
+            block.timestamp >= startTime && block.timestamp <= endTime,
+            "Voting is closed."
+        );
         if (opinions[msg.sender].length == 0) {
             hackers.push(msg.sender);
         }
